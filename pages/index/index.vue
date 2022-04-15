5727a33dbf28c69d3ba7333bf997361d74ac4518
cwm <1260027555@qq.com>
2022-04-15 17:58:37 +0800

    '初始化项目'

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<text class="iconfont iconfaxian" style="font-size: 100rpx;"></text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
