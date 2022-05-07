ed4178bcc45f55ae55db7b04031fee48bb7b69b7
jacky <jacky@qq.com>
2022-05-08 00:48:29 +0800

    更新

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<view>
+	<view class="flex flex-column" style="min-height: 100vh;">
 		<!-- 轮播图部分 -->
 		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="200"
 			style="width: 750rpx;height: 250rpx;">
@@ -12,38 +12,50 @@
 		</swiper>
 
 		<!-- 直播列表 -->
-		<view class="flex flex-wrap">
-			<view class="p position-relative list-item" v-for="(item,index) in list" :key="index" @tap="openLive">
-				<image class="rounded" src="/static/demo/1.jpg" mode="aspectFill"
-					style="width: 365rpx;height: 365rpx;">
-				</image>
-				<view class="rounded-circle position-absolute px-2 flex align-center"
-					style="left: 15rpx;top: 15rpx;background-color: rgba(0,0,0,.4);">
-					<text class="iconfont iconbizhongguanli text-warning mr-1"></text>
-					<text class="text-white font">{{item.coin}}</text>
-				</view>
-				<view class="rounded-circle position-absolute px-2 flex align-center"
-					style="right: 15rpx;top: 15rpx;background-color: rgba(0,0,0,.4);">
-					<text class="text-white font-sm mr-1">人气:</text>
-					<text class="text-white font-sm">{{item.look_count}}</text>
-				</view>
-				<view class="rounded-circle position-absolute flex align-center" style="left: 15rpx;bottom: 15rpx;">
-					<text class="text-white font-sm">{{item.title}}</text>
-				</view>
-				<view class="rounded-circle position-absolute px-2 flex align-center"
-					style="right: 15rpx;bottom: 15rpx;background-color: rgba(0,0,0,.4);">
-					<text class="d-block rounded-circle mr-1" :class="item.status===1?'bg-success':'bg-danger'"
-						style="width: 20rpx;height: 20rpx;"></text>
-					<text class="text-white font-sm">{{item.status | formatStatus}}</text>
+		<template v-if="list.length>0 && !loading">
+			<view class="flex flex-wrap">
+				<view class="p position-relative list-item" v-for="(item,index) in list" :key="index" @tap="openLive">
+					<image class="rounded" src="/static/demo/1.jpg" mode="aspectFill"
+						style="width: 365rpx;height: 365rpx;">
+					</image>
+					<view class="rounded-circle position-absolute px-2 flex align-center"
+						style="left: 15rpx;top: 15rpx;background-color: rgba(0,0,0,.4);">
+						<text class="iconfont iconbizhongguanli text-warning mr-1"></text>
+						<text class="text-white font">{{item.coin}}</text>
+					</view>
+					<view class="rounded-circle position-absolute px-2 flex align-center"
+						style="right: 15rpx;top: 15rpx;background-color: rgba(0,0,0,.4);">
+						<text class="text-white font-sm mr-1">人气:</text>
+						<text class="text-white font-sm">{{item.look_count}}</text>
+					</view>
+					<view class="rounded-circle position-absolute flex align-center" style="left: 15rpx;bottom: 15rpx;">
+						<text class="text-white font-sm">{{item.title}}</text>
+					</view>
+					<view class="rounded-circle position-absolute px-2 flex align-center"
+						style="right: 15rpx;bottom: 15rpx;background-color: rgba(0,0,0,.4);">
+						<text class="d-block rounded-circle mr-1" :class="item.status===1?'bg-success':'bg-danger'"
+							style="width: 20rpx;height: 20rpx;"></text>
+						<text class="text-white font-sm">{{item.status | formatStatus}}</text>
+					</view>
 				</view>
 			</view>
-		</view>
 
-		<!-- 没有更多了 -->
-		<view class="f-divider"></view>
-		<view class="flex align-center justify-center py-3">
-			<text class="font-md text-secondary">{{loadText}}</text>
-		</view>
+			<!-- 没有更多了 -->
+			<view class="f-divider"></view>
+			<view class="flex align-center justify-center py-3">
+				<text class="font-md text-secondary">{{loadText}}</text>
+			</view>
+		</template>
+		<template v-else-if="loading">
+			<view class="flex-1 flex justify-center align-center border">
+				<text>加载中...</text>
+			</view>
+		</template>
+		<template v-else>
+			<view class="flex-1 flex justify-center align-center border">
+				<text>暂无数据</text>
+			</view>
+		</template>
 	</view>
 </template>
 
@@ -53,7 +65,8 @@
 			return {
 				list: [],
 				page: 1, //列表当前页码
-				loadText: "上拉加载更多"
+				loadText: "上拉加载更多",
+				loading: false
 			}
 		},
 		onLoad() {
@@ -88,10 +101,15 @@
 		},
 		methods: {
 			async getData(callback = false, refresh = false) {
+				// 首次进入首页才会显示加载中...字样
+				if (this.list.length === 0) {
+					this.loading = true
+				}
 				let res = await this.$H.get(`/live/list/${this.page}`)
-				// console.log('res', res)
-				
+				this.loading = false
+
 				this.list = refresh ? res : [...this.list, ...res]
+
 				this.loadText = res.length < 10 ? '没有更多了' : '上拉加载更多'
 
 				if (typeof callback === 'function') {
