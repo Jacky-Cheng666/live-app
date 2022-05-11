e8e1632df253daa398db29c17264d41513b13b3a
cwm <1260027555@qq.com>
2022-05-11 15:16:59 +0800

    更新

--- common/config.js
+++ common/config.js
@@ -2,6 +2,8 @@ export default {
 	baseUrl: 'http://192.168.1.206:7001',
 	// socket服务器地址
 	socketUrl: 'http://192.168.1.206:7001',
-	// 直播服务器地址
-	liveUrl: 'http://192.168.1.206:23481/live/'
+	// 拉流服务器地址
+	pullBaseUrl: 'http://192.168.1.206:23481/live/',
+	// 推流服务器地址
+	pushBaseUrl: 'rtmp://192.168.1.206:23480/live/'
 }
